08e72a41660a147e0231e1c7115a91ab5a725cc1
miwsirilak <sirilak.yo.60@ubu.ac.th>
2021-01-14 19:01:10 +0700

    admin&user

--- app/Http/Livewire/User/UserDashboardComponent.php
+++ app/Http/Livewire/User/UserDashboardComponent.php
@@ -9,6 +9,6 @@ class UserDashboardComponent extends Component
     public function render()
     {
         // return view('livewire.user.user-dashboard-component')->layout('layouts.base');
-        return view('livewire.user.user-dashboard-component')->layout('page_user.index');
+        return view('livewire.user.user-dashboard-component')->layout('page_user.location');
     }
 }
